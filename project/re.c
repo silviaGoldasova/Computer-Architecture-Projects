1b3b7466f3391715f39d3f816abeb8728a0579ab
Goldasová, Silvia <goldasil@fel.cvut.cz>
2020-06-05 15:11:56 +0200

    Add increasing length fuction when eating food

--- project/re.c
+++ project/re.c
@@ -2,7 +2,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <strings.h>
 
 #include <stdint.h>
 #include <time.h>
@@ -10,8 +9,6 @@
 #include <stdbool.h>
 #include <termios.h>
 #include <unistd.h>
-
-#include <sys/select.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -34,13 +31,13 @@
 #define SNAKE_COLOR 0xFFFF
 #define BORDER_COLOR 0xFFFE
 #define BORDER_SIZE 10
-#define APPLES_COUNT 25
+#define APPLES_COUNT 7
 
-typedef struct Cell{
+typedef struct CellDirection{
 	int posX;
 	int posY;
 	unsigned char direction; 
-} Cell;
+} CellDirection;
 
 
 static void setSerialPort() {
@@ -54,7 +51,6 @@ static void setSerialPort() {
     tcgetattr(fd,&oldtio); // save current port settings
 
 	bzero(&newtio, sizeof(newtio));
-	
 	newtio.c_cflag = CS8 | CREAD;
 	newtio.c_iflag = IGNPAR;
 	newtio.c_oflag = 0;
@@ -62,10 +58,8 @@ static void setSerialPort() {
 	// set input mode (non-canonical, no echo,...)
 	newtio.c_lflag = 0;
 		
-	newtio.c_cc[VTIME] = 75; /* Set timeout of 10.0 seconds */
-	//newtio.c_cc[VTIME]    = 0;   // inter-character timer unused
-	//newtio.c_cc[VMIN]     = 1;   // blocking read until 1 char received
-	newtio.c_cc[VMIN]     = 0;   // blocking read until 1 char received
+	newtio.c_cc[VTIME]    = 0;   // inter-character timer unused
+	newtio.c_cc[VMIN]     = 1;   // blocking read until 1 char received
 	
 	tcflush(fd, TCIFLUSH);
 	tcsetattr(fd,TCSANOW,&newtio);
@@ -73,7 +67,7 @@ static void setSerialPort() {
 	/*while (STOP==FALSE) {       // loop for input 
 		res = read(fd,buf,255);   // returns after 5 chars have been input 
 		buf[res]=0;               // so we can printf... 
-		printf(":%s:%d\r\n", buf, res);
+		printf(":%s:%d\n", buf, res);
 		if (buf[0]=='z') STOP=TRUE;
 	}*/
 	//tcsetattr(fd,TCSANOW,&oldtio);
@@ -104,7 +98,7 @@ bool isInRange(int currentRow, int currentCol, int pointX, int pointY, int range
 	return false;
 }
 
-void initializeSnakeAndDirection(int initX, int initY, int snakeLength, uint16_t * snakeArr, Cell ** directionArr) {
+void initializeSnakeAndDirection(int initX, int initY, int snakeLength, uint16_t * snakeArr, CellDirection ** directionArr) {
 
 	int distance = CELL_SIZE / 2;
 
@@ -113,8 +107,8 @@ void initializeSnakeAndDirection(int initX, int initY, int snakeLength, uint16_t
 	int bottomRightCornerX = upperLeftCornerX + snakeLength * CELL_SIZE;
 	int bottomRightCornerY = upperLeftCornerY + CELL_SIZE;
 
-	//printf("pos initial (upper left point): [%d, %d]\r\n", upperLeftCornerX, upperLeftCornerY);
-	//printf("pos initial (bottom right point): [%d, %d]\r\n", bottomRightCornerX, bottomRightCornerY);
+	//printf("pos initial (upper left point): [%d, %d]\n", upperLeftCornerX, upperLeftCornerY);
+	//printf("pos initial (bottom right point): [%d, %d]\n", bottomRightCornerX, bottomRightCornerY);
 
 
 	for(int row = upperLeftCornerY; row < bottomRightCornerY; row++) {
@@ -125,7 +119,7 @@ void initializeSnakeAndDirection(int initX, int initY, int snakeLength, uint16_t
 	}
 
 	for (int i = 0; i < snakeLength; i++) {
-		directionArr[i] = (Cell *) malloc(sizeof(Cell));
+		directionArr[i] = (CellDirection *) malloc(sizeof(CellDirection));
 		directionArr[i] -> posX = initX; 
 		directionArr[i] -> posY = initY;
 		directionArr[i] -> direction = 'R';
@@ -208,7 +202,7 @@ void distributeApples(uint16_t * snakeArr){
 
 }
 
-void shiftDirCell(Cell * cell, unsigned char prevCellDir) {
+void shiftDirCell(CellDirection * cell, unsigned char prevCellDir) {
 	switch (cell -> direction){
 		case 'U':
 			cell -> posY = cell -> posY - CELL_SIZE;
@@ -233,10 +227,10 @@ bool isWithinLCD(int posX, int posY){
 	return true;
 }
 
-bool isCellOccupied(uint16_t * snakeArr, Cell * head){
+bool isCellOccupied(uint16_t * snakeArr, CellDirection * head){
 
 	int posInSnakeArr = (head->posY) * 480 + head->posX;
-	printf("pos of head: [%d, %d], color: %hu\r\n", head->posX, head->posY, snakeArr[posInSnakeArr]);
+	printf("pos of head: [%d, %d], color: %hu\n", head->posX, head->posY, snakeArr[posInSnakeArr]);
 
 	if (snakeArr[posInSnakeArr] == SNAKE_COLOR) {
 		return true;
@@ -245,11 +239,12 @@ bool isCellOccupied(uint16_t * snakeArr, Cell * head){
 
 }
 
-bool isApple(uint16_t * snakeArr, Cell * head) {
+bool isApple(uint16_t * snakeArr, CellDirection * head) {
 
 	int posInSnakeArr = (head->posY) * 480 + head->posX;
 
-	if (snakeArr[posInSnakeArr] == (uint16_t) APPLE_COLOR) {
+	if (snakeArr[posInSnakeArr] == APPLE_COLOR) {
+		printf("found an apple: %hu\n", snakeArr[posInSnakeArr]);
 		return true;
 	}
 
@@ -257,28 +252,18 @@ bool isApple(uint16_t * snakeArr, Cell * head) {
 
 }
 
-bool snakeMakeMove(uint16_t * snakeArr, Cell ** directionArr, int * length) {
+bool snakeMakeMove(uint16_t * snakeArr, CellDirection ** directionArr, int * length) {
 
-	Cell * head = directionArr[*length-1];
-	int headPosX = head->posX;
-	int headPosY = head->posY;
+	CellDirection * head = directionArr[*length-1];
 
 	if (!(isWithinLCD(head->posX, head->posY))) {
 		return false;
 	}
-	
-	shiftDirCell(head, head->direction);	// shift head cell, direction stays
-	if (isApple(snakeArr, head)){
-		redrawSnakeCell(snakeArr, head->posX, head->posY, 0xFFFF);
-		
-		directionArr[*length] = directionArr[*length-1];
-		directionArr[*length-1] = (Cell *) malloc(sizeof(Cell));
-		directionArr[*length-1] -> posX = headPosX; 
-		directionArr[*length-1] -> posY = headPosY;
-		directionArr[*length-1] -> direction = directionArr[*length]->direction;
 
+	if (isApple(snakeArr, head)){
+		shiftDirCell(head, head->direction);
+		redrawSnakeCell(snakeArr, head->posX, head->posY, 0xFFFF);
 		*length = *length + 1;
-		printf("Apple eaten -> updated length: %d\r\n", *length);
 		return true;
 	}
 
@@ -289,17 +274,19 @@ bool snakeMakeMove(uint16_t * snakeArr, Cell ** directionArr, int * length) {
 	for (int i = 0; i < *length-1; i++) {
 		shiftDirCell(directionArr[i], directionArr[i+1] -> direction);		// shift a cell
 	}
-	
+
+	shiftDirCell(head, head->direction);	// shift head cell, direction stays
 	if (isCellOccupied(snakeArr, head)) {		// check if in snake array is blank position for the pos to which head is moved
 		return false;
 	}
 
 	redrawSnakeCell(snakeArr, head->posX, head->posY, 0xFFFF);
+
 	return true;
 
 } 
 
-void updateDirection(Cell * cell, unsigned char newDirection) {
+void updateDirection(CellDirection * cell, unsigned char newDirection) {
 	cell -> direction = newDirection;
 }
 
@@ -339,104 +326,6 @@ unsigned char getRandomDirection(unsigned char currentDir) {
 
 }
 
-void setReadTimeout() {
-
-	/*fd_set selectset;
-	struct timeval timeout = {3.5,0}; //timeout of 10 secs.
-	FD_ZERO(&selectset);
-	FD_SET(0,&selectset);
-	int ret =  select(1, &selectset, NULL, NULL, &timeout);*/
-
-	//////////////////////////
-
-	/*char c;
-	if (read(0, &c, 1) == 1) {
-		blackLcd(parlcd_mem_base);
-		printf("c:.%c.\r\n", c);
-		//write(1, &c, 1);
-	}*/
-	
-	//setReadTimeout();
-
-	/*fd_set selectset;
-	struct timeval timeout = {3.5,0}; //timeout of 10 secs.
-	FD_ZERO(&selectset);
-	FD_SET(0,&selectset);
-	int ret =  select(1, &selectset, NULL, NULL, &timeout);*/
-	/*if(ret == 0)	// no data
-		printf("No data\r\n");
-	else if(ret == -1)	//error
-		printf("Error\r\n");
-	else  {
-		printf("Data avalable: ");
-		char c;
-		int ret = read(0, &c, 1);
-		printf("ret: %d, %c\r\n", ret, c);
-	}*/
-	// stdin has data, read it
-	// (we know stdin is readable, since we only asked for read events
-	//and stdin is the only fd in our select set.
-
-}
-
-unsigned char mapKeyToDirection(unsigned char lastDirection, char pressedKey) {
-
-	// press 'a' for left, 'd' for right
-
-	switch(lastDirection) {
-		case 'R':
-			if (pressedKey == 'a')	// left
-				return 'U';
-			else	// right
-				return 'D';
-			break;
-		case 'L':
-			if (pressedKey == 'a')	// left
-				return 'D';
-			else	// right
-				return 'U';
-			break;
-		case 'U':
-			if (pressedKey == 'a')	// left
-				return 'L';
-			else	// right
-				return 'R';
-			break;
-		case 'D':
-			if (pressedKey == 'a')	// left
-				return 'R';
-			else	// right
-				return 'L';
-			break;
-	}
-	printf("pressedKey not identified");
-	return 'X';
-
-}
-
-unsigned char getKeyboardInput(unsigned char lastDirection){
-
-	char keyPressed;
-	int ret = read(0, &keyPressed, 1);
-	if (ret != 1)
-		return lastDirection;
-	
-	//printf("ret: %d, %c\r\n", ret, keyPressed);
-
-	if (!(keyPressed == 'a' || keyPressed == 'd')) {
-		if (keyPressed == 'e')
-			return 'e';
-		printf("other key\r\n");
-		return lastDirection;
-	}
-	
-	unsigned char newDir = mapKeyToDirection(lastDirection, keyPressed);
-
-	printf("Updating cell head direction: from %c to %c\r\n", lastDirection, newDir);
-	return newDir;
-
-}
-
 int main(int argc, char *argv[]) {
 	
 	time_t t;
@@ -446,12 +335,11 @@ int main(int argc, char *argv[]) {
   	unsigned char *parlcd_mem_base;
   	//unsigned int c;
   	uint16_t * snake = (uint16_t *) calloc(480*320, sizeof(uint16_t)); 
-	//Cell ** directionArr = (Cell **) calloc(480*320, sizeof(Cell *));   
-	Cell * directionArr[480*320];
+	//CellDirection ** directionArr = (CellDirection **) calloc(480*320, sizeof(CellDirection *));   
+	CellDirection * directionArr[480*320];
 	int snakeLength = 5;
-	setSerialPort();
 
-	printf("Start\r\n");
+	printf("Start\n");
 	//Setup memory mapping which provides access to the peripheral  registers region of RGB LEDs, knobs and line of yellow LEDs.
 	mem_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
 	if (mem_base == NULL) exit(1);
@@ -467,20 +355,24 @@ int main(int argc, char *argv[]) {
 	bool isGameOver = false;
 	unsigned char currentDir = 'R';
 
-
 	while (!(isGameOver)) {
 		printSnakeToLcd(snake, parlcd_mem_base);
-		sleep(0.5);
+		sleep(0.7);
 
-		//currentDir = getRandomDirection(currentDir);
-		currentDir = getKeyboardInput(currentDir);
-
-		if (currentDir == 'e') break; 
+		currentDir = getRandomDirection(currentDir);
 		updateDirection(directionArr[snakeLength-1], currentDir);
+		printf("current dir: %c ", currentDir);
 		
 		if (!(snakeMakeMove(snake, directionArr, &snakeLength))) {
 			isGameOver = true;
 		}
+	}
+	
+	setSerialPort();
+	char c;
+	if (read(0, &c, 1) == 1) {
+		printf("%c", c);
+		write(1, &c, 1);
 	}
 
 	sleep(2);
@@ -494,7 +386,8 @@ int main(int argc, char *argv[]) {
 	}
 
 	free(snake);
-  	printf("\r\nEnd\r\n");
+	//free(directionArr);
+  	printf("End\n");
 
   	return 0;
 }
